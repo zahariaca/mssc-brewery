987a5c6340f42a889d49ceabef894fb58208745f
zahariaca <zaharia.c.alexandru@gmail.com>
2021-02-20 16:49:10 +0200

    Section 6 validation externalized to @ControllerAdvice annotated class

---
+++ src/main/java/guru/springframework/msscbrewery/web/controller/McxExceptionHandler.java
@@ -0,0 +1,31 @@
+package guru.springframework.msscbrewery.web.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import javax.validation.ConstraintViolationException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+
+@ControllerAdvice
+public class McxExceptionHandler {
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<List<String>> errorHandler(ConstraintViolationException e) {
+        List<String> errors = new ArrayList<>(e.getConstraintViolations().size());
+
+        e.getConstraintViolations().forEach(constraintViolation -> {
+            errors.add(constraintViolation.getPropertyPath() + ":" + constraintViolation.getMessage());
+        });
+        return new ResponseEntity<>(errors, BAD_REQUEST);
+    }
+
+    @ExceptionHandler(BindException.class)
+    public ResponseEntity<List<ObjectError>> handleBindException(BindException e) {
+        return new ResponseEntity<>(e.getAllErrors(), BAD_REQUEST);
+    }
+}
